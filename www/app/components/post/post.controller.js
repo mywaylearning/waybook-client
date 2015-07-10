3ddc59f85eef3609b370547bf6bce8595bdc3e9c
Igor Prado <igorprado@gmail.com>
2015-07-10 16:01:37 -0300

    Add link to post behaviour (fetching data from URL)

--- www/app/components/post/post.controller.js
+++ www/app/components/post/post.controller.js
@@ -4,21 +4,70 @@
 
   var debug = require('debug')('waybook:PostController');
 
-  function PostController($scope, router, goal, SWAGGER) {
+  function PostController($scope, router, goal, SWAGGER, PostService) {
     debug('here we are (directive controller)');
 
     debug(SWAGGER);
 
     var ctrl = this;
 
+    function init() {
+      // Define type of post mode (show form)
+      ctrl.postMode = 'link';
+
+      // Other definitions
+      ctrl.placeHolder = "Share something...";
+
+      debug('type...? ' + ctrl.postType);
+    }
+    init();
+
+    $scope.$watch('ctrl.postMode', function(newValue) {
+      if (newValue === false) {
+        ctrl.extractLinkUrl = null;
+        ctrl.extractResult = null;
+      }
+    });
+
+    ctrl.extractLink = function() {
+      PostService.extractLink(ctrl.extractLinkUrl).then(function(result){
+        ctrl.extractLinkUrl = '';
+        var image, imageType, imageLandscape = false;
+
+        if (result.images.length) {
+          var _image = result.images[0];
+          image = _image.url;
+          imageType = 'image';
+
+          if ((_image.width / 1.1) > _image.height) {
+            imageLandscape = true;
+          }
+
+        } else if (result.favicon_url) {
+          image = result.favicon_url;
+          imageType = 'icon';
+        } else {
+          image = false;
+        }
+        ctrl.extractResult = {
+          title: result.title,
+          description: result.description,
+          image: image,
+          imageType: imageType,
+          imageLandscape: imageLandscape,
+          url: result.url
+        };
+      });
+    };
+
+
+
+
     ctrl.model = {
       title: 'New Goal'
     };
 
-    function init() {
-      debug('type...? ' + ctrl.postType);
-    }
-    init();
+
 
 
     ctrl.save = function() {
@@ -28,6 +77,6 @@
 
   }
 
-  module.exports = ['$scope', 'router', 'goal', 'SWAGGER', PostController];
+  module.exports = ['$scope', 'router', 'goal', 'SWAGGER', 'PostService', PostController];
 
 }());

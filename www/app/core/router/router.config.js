c5fdfca36dd06dd225176750f34d2e62b037e200
lesterzone <lesterzone@gmail.com>
2015-07-30 15:17:08 -0600

    Register url to signup and set route
    Set route to sign up and dependency added to handle register action

--- www/app/core/router/router.config.js
+++ www/app/core/router/router.config.js
@@ -1,240 +1,264 @@
-(function() {
+'use strict';
 
-  'use strict';
+var debug = require('debug')('waybook:RouterConfig');
+var fs = require('fs');
 
-  var debug = require('debug')('waybook:RouterConfig');
-  var fs = require('fs');
+require('ionic');
 
-  require('ionic');
+function RouterConfig($stateProvider, $urlRouterProvider, $urlMatcherFactoryProvider, $locationProvider, store, ROLES, LOCAL_STORAGE_KEYS) {
+  var userResolve, guestResolve, key;
 
-  function RouterConfig($stateProvider, $urlRouterProvider, $urlMatcherFactoryProvider, $locationProvider, store, ROLES, LOCAL_STORAGE_KEYS) {
-    var userResolve, guestResolve, key;
+  $urlMatcherFactoryProvider.strictMode(false);
+  $urlMatcherFactoryProvider.caseInsensitive(true);
+  $locationProvider.html5Mode(false);
+  $locationProvider.hashPrefix('!');
 
-    $urlMatcherFactoryProvider.strictMode(false);
-    $urlMatcherFactoryProvider.caseInsensitive(true);
-    $locationProvider.html5Mode(false);
-    $locationProvider.hashPrefix('!');
+  userResolve = {
+    userGrant: function(grant) {
+      return grant.only([ROLES.user], ['app.dashboard']);
+    }
+  };
 
-    userResolve = {
-      userGrant: function(grant) {
-        return grant.only([ROLES.user], ['app.dashboard']);
-      }
-    };
+  guestResolve = {
+    guestGrant: function(grant) {
+      return grant.only([ROLES.guest], ['public.login']);
+    }
+  };
 
-    guestResolve = {
-      guestGrant: function(grant) {
-        return grant.only([ROLES.guest], ['public.login']);
-      }
-    };
+  $stateProvider
 
-    $stateProvider
-
-    /**
-     * CUSTOM DECORATOR
-     * Because life is too short to type in full paths to template files.
-     */
+  /**
+   * CUSTOM DECORATOR
+   * Because life is too short to type in full paths to template files.
+   */
     .decorator('views', function(state, parent) {
-      var result = {};
-      var views = parent(state);
+    var result = {};
+    var views = parent(state);
 
-      var platform = '';
-      // If we ever need platform-specific views, we'll use this
-      // if (state.data && state.data.platformVariants !== undefined) {
-      //   if (ionic.Platform.isAndroid()) {
-      //     platform = '-droid-';
-      //   } else if (ionic.Platform.isIOS()) {
-      //     platform = '-ios-';
-      //   }
-      // }
+    var platform = '';
 
-      // if a templateUrl is already defined above the view, then we can skip all this.
-      if (state.templateUrl) {
-        return views;
-      }
-
-      // if it's abstract, we can skip it also
-      if (state.abstract) {
-        return views;
-      }
-
-      var stateNameParts = state.name.split('.');
-      var baseDir = '/' + stateNameParts[0];
-      var section = '';
-      var sectionDir = '';
-      if (stateNameParts.length === 2) {
-        sectionDir = '/' + stateNameParts[1];
-        section = stateNameParts[1];
-      }
-
-      angular.forEach(views, function(config, name) {
-        if (config.templateUrl) {
-          result[name] = config;
-        } else {
-          var viewNameParts = name.split('@');
-          config.templateUrl = '/app/sections' + sectionDir + '/' + section + '.' + viewNameParts[0] + platform + '.html';
-          result[name] = config;
-        }
-      });
-
-      return result;
-    })
-
-    .state('public', {
-      abstract: true,
-      //templateUrl: 'app/sections/app/public-base.html',
-      template: '<ion-nav-view name="publicContent"></ion-nav-view>',
-      controller: function($scope, currentUser) {
-        $scope.app.user = currentUser;
-      },
-      resolve: {
-        currentUser: function(user) {
-          return user.currentUser();
-        }
-      }
-    })
-
-
-    .state('public.login', {
-      url: '^/login',
-      // templateUrl: 'app/sections/login/login.html',
-      // controller: 'LoginController'
-      views: {
-        'publicContent': {
-          templateUrl: 'app/sections/login/login.publicContent.html',
-          controller: 'LoginController'
-        }
-      }
-    })
-
-    .state('public.intro', {
-      url: '^/intro',
-      views: {
-        'publicContent': {
-          templateUrl: 'app/sections/intro/intro.publicContent.html',
-          controller: 'IntroController'
-        }
-      }
-    })
-
-    .state('app', {
-      abstract: true,
-      templateUrl: 'app/sections/app/base.html',
-      controller: function(app) {
-        app.setUser();
-      },
-      resolve: userResolve
-    })
-
-    // .state('app.logout', {
-    //   url: '^/logout',
-    //   templateUrl: 'app/sections/app/logout.html',
-    //   controller: function($scope) {
-    //     debug($scope);
-    //     $scope.logout();
+    // If we ever need platform-specific views, we'll use this
+    // if (state.data && state.data.platformVariants !== undefined) {
+    //   if (ionic.Platform.isAndroid()) {
+    //     platform = '-droid-';
+    //   } else if (ionic.Platform.isIOS()) {
+    //     platform = '-ios-';
     //   }
-    // })
+    // }
 
-    .state('app.discover', {
-      abstract: true,
-      views: {
-        'bodyContent': {
-          template: '<ion-nav-view name="discover-bodyContent"></ion-nav-view>'
-        }
-      }
-    })
+    // if a templateUrl is already defined above the view, then we can skip all this.
+    if (state.templateUrl) {
+      return views;
+    }
 
-    .state('app.discover.entry', {
-      url: '^/discover',
-      views: {
-        'discover-bodyContent': {
-          templateUrl: 'app/sections/discover/discover.bodyContent.html',
-          controller: 'DiscoverController'
-        }
-      }
-    })
+    // if it's abstract, we can skip it also
+    if (state.abstract) {
+      return views;
+    }
 
-    .state('app.discover.exploration', {
-      url: '^/discover/:category/:exploration',
-      views: {
-        'discover-bodyContent': {
-          templateUrl: 'app/sections/discover/exploration.bodyContent.html',
-          controller: 'ExplorationController'
-        }
-      }
-    })
+    var stateNameParts = state.name.split('.');
+    var baseDir = '/' + stateNameParts[0];
+    var section = '';
+    var sectionDir = '';
+    if (stateNameParts.length === 2) {
+      sectionDir = '/' + stateNameParts[1];
+      section = stateNameParts[1];
+    }
 
-    .state('app.plan', {
-      cache: false,
-      url: '^/plan',
-      views: {
-        'bodyContent': { controller: 'PlanController' }
-      }
-    })
-
-
-    .state('app.unite', {
-      url: '^/unite',
-      views: {
-        'bodyContent': { controller: 'UniteController' }
-      }
-    })
-
-    .state('app.me', {
-      url: '^/me',
-      views: {
-        'bodyContent': { controller: 'MeController' }
-      }
-    })
-
-    .state('app.help-feedback', {
-      url: '^/help-feedback',
-      views: {
-        'bodyContent': { controller: 'HelpFeedbackController' }
-      }
-    })
-
-    .state('app.about', {
-      url: '^/about',
-      views: {
-        'bodyContent': { controller: 'AboutController' }
-      }
-    })
-
-    .state('app.dashboard', {
-      url: '^/dashboard',
-      views: {
-        'bodyContent': { controller: 'DashboardController' }
+    angular.forEach(views, function(config, name) {
+      if (config.templateUrl) {
+        result[name] = config;
+      } else {
+        var viewNameParts = name.split('@');
+        config.templateUrl = '/app/sections' + sectionDir + '/' + section + '.' + viewNameParts[0] + platform + '.html';
+        result[name] = config;
       }
     });
 
-    // .state('app.search', {
-    //   url: '^/search',
-    //   views: {
-    //     'bodyContent': { controller: 'SearchController' }
-    //   }
-    // })
-    //
-    //
-    //
-    // .state('app.single', {
-    //   url: '^/playlists/:playlistId',
-    //   views: {
-    //     'bodyContent': { controller: 'PlaylistController' }
-    //   }
-    // });
-    // if none of the above states are matched, use this as the fallback
-    key = LOCAL_STORAGE_KEYS.introSeen;
+    return result;
+  })
 
-    if (store.get(key)) {
-      $urlRouterProvider.otherwise('/login');
-    } else {
-      $urlRouterProvider.otherwise('/intro');
+  .state('public', {
+    abstract: true,
+    //templateUrl: 'app/sections/app/public-base.html',
+    template: '<ion-nav-view name="publicContent"></ion-nav-view>',
+    controller: function($scope, currentUser) {
+      $scope.app.user = currentUser;
+    },
+    resolve: {
+      currentUser: function(user) {
+        return user.currentUser();
+      }
     }
+  })
 
+  .state('public.login', {
+    url: '^/login',
+    views: {
+      'publicContent': {
+        templateUrl: 'app/sections/login/login.publicContent.html',
+        controller: 'LoginController'
+      }
+    }
+  })
+
+  .state('public.register', {
+    url: '^/register',
+    views: {
+      'publicContent': {
+        templateUrl: 'app/sections/register/register.publicContent.html',
+        controller: 'RegisterController'
+      }
+    }
+  })
+
+  .state('public.intro', {
+    url: '^/intro',
+    views: {
+      'publicContent': {
+        templateUrl: 'app/sections/intro/intro.publicContent.html',
+        controller: 'IntroController'
+      }
+    }
+  })
+
+  .state('app', {
+    abstract: true,
+    templateUrl: 'app/sections/app/base.html',
+    controller: function(app) {
+      app.setUser();
+    },
+    resolve: userResolve
+  })
+
+  // .state('app.logout', {
+  //   url: '^/logout',
+  //   templateUrl: 'app/sections/app/logout.html',
+  //   controller: function($scope) {
+  //     debug($scope);
+  //     $scope.logout();
+  //   }
+  // })
+
+  .state('app.discover', {
+    abstract: true,
+    views: {
+      'bodyContent': {
+        template: '<ion-nav-view name="discover-bodyContent"></ion-nav-view>'
+      }
+    }
+  })
+
+  .state('app.discover.entry', {
+    url: '^/discover',
+    views: {
+      'discover-bodyContent': {
+        templateUrl: 'app/sections/discover/discover.bodyContent.html',
+        controller: 'DiscoverController'
+      }
+    }
+  })
+
+  .state('app.discover.exploration', {
+    url: '^/discover/:category/:exploration',
+    views: {
+      'discover-bodyContent': {
+        templateUrl: 'app/sections/discover/exploration.bodyContent.html',
+        controller: 'ExplorationController'
+      }
+    }
+  })
+
+  .state('app.plan', {
+    cache: false,
+    url: '^/plan',
+    views: {
+      'bodyContent': {
+        controller: 'PlanController'
+      }
+    }
+  })
+
+
+  .state('app.unite', {
+    url: '^/unite',
+    views: {
+      'bodyContent': {
+        controller: 'UniteController'
+      }
+    }
+  })
+
+  .state('app.me', {
+    url: '^/me',
+    views: {
+      'bodyContent': {
+        controller: 'MeController'
+      }
+    }
+  })
+
+  .state('app.help-feedback', {
+    url: '^/help-feedback',
+    views: {
+      'bodyContent': {
+        controller: 'HelpFeedbackController'
+      }
+    }
+  })
+
+  .state('app.about', {
+    url: '^/about',
+    views: {
+      'bodyContent': {
+        controller: 'AboutController'
+      }
+    }
+  })
+
+  .state('app.dashboard', {
+    url: '^/dashboard',
+    views: {
+      'bodyContent': {
+        controller: 'DashboardController'
+      }
+    }
+  });
+
+  // .state('app.search', {
+  //   url: '^/search',
+  //   views: {
+  //     'bodyContent': { controller: 'SearchController' }
+  //   }
+  // })
+  //
+  //
+  //
+  // .state('app.single', {
+  //   url: '^/playlists/:playlistId',
+  //   views: {
+  //     'bodyContent': { controller: 'PlaylistController' }
+  //   }
+  // });
+  // if none of the above states are matched, use this as the fallback
+  key = LOCAL_STORAGE_KEYS.introSeen;
+
+  if (store.get(key)) {
+    $urlRouterProvider.otherwise('/login');
+  } else {
+    $urlRouterProvider.otherwise('/intro');
   }
 
-  RouterConfig.$inject = ['$stateProvider', '$urlRouterProvider', '$urlMatcherFactoryProvider', '$locationProvider', 'store', 'ROLES', 'LOCAL_STORAGE_KEYS'];
+}
 
-  module.exports = RouterConfig;
+RouterConfig.$inject = [
+  '$stateProvider',
+  '$urlRouterProvider',
+  '$urlMatcherFactoryProvider',
+  '$locationProvider',
+  'store',
+  'ROLES',
+  'LOCAL_STORAGE_KEYS'
+];
 
-}());
+module.exports = RouterConfig;
--- www/app/core/user/user.service.js
+++ www/app/core/user/user.service.js
@@ -1,143 +1,132 @@
-(function() {
+'use strict';
 
-  'use strict';
+var debug = require('debug')('waybook:UserService');
 
-  var debug = require('debug')('waybook:UserService');
+function UserService(api, auth, router, utils, EVENTS, API_URL) {
+  var svcInterface, Users, User, userObj, userRequest;
 
-  function UserService(api, auth, router, utils, EVENTS, API_URL) {
-    var svcInterface, Users, User, userObj, userRequest;
+  Users = api.all('users');
+  User = api.one('user');
 
-    Users = api.all('users');
-    User = api.one('user');
+  userObj = null;
+  userRequest = null;
 
-    userObj = null;
-    userRequest = null;
+  /**
+   * Public
+   */
+  return {
+    register: _register,
+    currentUser: _currentUser,
+    getByUsername: _getByUsername,
+    getUploadImageUrl: _getUploadImageUrl,
+    getSelf: _getSelf,
+    updateSelf: _updateSelf,
+    logout: _logout,
+    test: function() {
+      return userObj;
+    }
+  };
 
-    /**
-     * Public
-     */
-    svcInterface = {
-      register: _register,
-      currentUser: _currentUser,
-      getByUsername: _getByUsername,
-      getUploadImageUrl: _getUploadImageUrl,
-      getSelf: _getSelf,
-      updateSelf: _updateSelf,
-      logout: _logout,
-      test: function() {
-        return userObj;
-      }
-    };
-    return svcInterface;
+  /**
+   * Helper method to retreive logged in user's information.
+   * Will first check to see that user is authenticated before
+   * trying to retreive the user object from API.
+   *
+   * @return {mixed} -  empty object if unautenticated
+   *                    Promise if authenticated
+   */
+  function _currentUser() {
+    return auth.isAuthenticated() ? _getSelf() : {};
+  }
 
-    /**
-     * Private
-     */
+  /**
+   * Will update the current user's info by
+   * making a PUT call to user API endpoint.
+   * @param  {Object} updates params to be updated.
+   * @return {Promise}
+   */
+  function _updateSelf(updates) {
+    var updateRequest;
 
-    /**
-     * Helper method to retreive logged in user's information.
-     * Will first check to see that user is authenticated before
-     * trying to retreive the user object from API.
-     *
-     * @return {mixed} -  empty object if unautenticated
-     *                    Promise if authenticated
-     */
-    function _currentUser() {
-      debug('in _currentUser');
-      //debug(auth);
-      var currentUser = auth.isAuthenticated() ? _getSelf() : {};
-      return currentUser;
+    updateRequest = User.customPUT(updates);
+    updateRequest.then(setUser);
+
+    return updateRequest;
+  }
+
+  function _getUploadImageUrl() {
+    return API_URL + '/user/image/upload';
+  }
+
+  /**
+   * Get public profile for user matching username.
+   * @param  {String} username
+   * @return {Promise}
+   */
+  function _getByUsername(username) {
+    return api.oneUrl('user', API_URL + '/profile/user/' + username).get();
+  }
+
+  /**
+   * Create a new user
+   * @param  {Object} user
+   * @return {Promise}
+   */
+  function _register(user) {
+    return Users.post(user);
+  }
+
+  /**
+   * Get the authenticated user's information.
+   * if userObj already exists mimic restangular promise and resolve with userObj
+   * @param  {Boolean} forceRefresh if true will force a fresh get user API call.
+   * @return {Promise}
+   */
+  function _getSelf(forceRefresh) {
+    if (userObj && !forceRefresh) {
+      return utils.promisify(userObj);
     }
 
-    /**
-     * Will update the current user's info by
-     * making a PUT call to user API endpoint.
-     * @param  {Object} updates params to be updated.
-     * @return {Promise}
-     */
-    function _updateSelf(updates) {
-      var updateRequest;
-
-      updateRequest = User.customPUT(updates);
-      updateRequest.then(setUser);
-
-      return updateRequest;
-    }
-
-    function _getUploadImageUrl() {
-      return API_URL + '/user/image/upload';
-    }
-
-    /**
-     * Get public profile for user matching username.
-     * @param  {String} username
-     * @return {Promise}
-     */
-    function _getByUsername(username) {
-      return api.oneUrl('user', API_URL + '/profile/user/' + username).get();
-    }
-
-    /**
-     * Create a new user
-     * @param  {Object} user
-     * @return {Promise}
-     */
-    function _register(user) {
-      return Users.post(user);
-    }
-
-    /**
-     * Get the authenticated user's information.
-     * if userObj already exists mimic restangular promise and resolve with userObj
-     * @param  {Boolean} forceRefresh if true will force a fresh get user API call.
-     * @return {Promise}
-     */
-    function _getSelf(forceRefresh) {
-      debug('in _getSelf');
-      if (userObj && !forceRefresh) {
-        return utils.promisify(userObj);
-      }
-
-      if (userRequest) {
-        return userRequest;
-      }
-
-      userRequest = User.customGET();
-      userRequest.then(setUser).finally(getUserComplete);
-
+    if (userRequest) {
       return userRequest;
     }
 
-    /**
-     * Logout out the authenticated user.
-     * Will delete the access token, user info,
-     * and redirect to logged out page.
-     */
-    function _logout() {
-      auth.destroy();
-      userObj = null;
-      userRequest = null;
+    userRequest = User.customGET();
+    userRequest.then(setUser).finally(getUserComplete);
 
-      router.goToLoggedOut();
-    }
-
-    /**
-     * Store user information in userObj
-     */
-    function setUser(user) {
-      userObj = user;
-    }
-
-    /**
-     * Clear all references to Restangular user elements
-     * used during get user calls.
-     */
-    function getUserComplete() {
-      userRequest = null;
-    }
-
+    return userRequest;
   }
 
-  module.exports = ['api', 'auth', 'router', 'utils', 'EVENTS', 'API_URL', UserService];
+  /**
+   * Logout out the authenticated user.
+   * Will delete the access token, user info,
+   * and redirect to logged out page.
+   */
+  function _logout() {
+    auth.destroy();
+    userObj = null;
+    userRequest = null;
 
-}());
+    router.goToLoggedOut();
+  }
+
+  /**
+   * Store user information in userObj
+   */
+  function setUser(user) {
+    userObj = user;
+  }
+
+  /**
+   * Clear all references to Restangular user elements
+   * used during get user calls.
+   */
+  function getUserComplete() {
+    userRequest = null;
+  }
+
+}
+
+module.exports = [
+  'api', 'auth', 'router', 'utils', 'EVENTS', 'API_URL', UserService
+];

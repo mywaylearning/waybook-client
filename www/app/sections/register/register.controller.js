5fa2c85bef8deea09d678432ecb7ee7a35f1c7ec
Igor Prado <igorprado@gmail.com>
2015-08-11 15:12:39 -0300

    Added modal to show verification message and improved form with labels

--- www/app/sections/register/register.controller.js
+++ www/app/sections/register/register.controller.js
@@ -1,8 +1,26 @@
 'use strict';
 
-function RegisterController($scope, router, user, errorHandler) {
+function RegisterController($scope, router, user, errorHandler, $ionicModal) {
 
   $scope.register = {};
+
+  // Creates a modal instance in case of registration is successfull
+  $ionicModal.fromTemplateUrl('app/sections/register/register.emailSent.html', {
+    scope: $scope,
+    animation: 'slide-in-up'
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+
+  // Redirect user when click on the modal button
+  $scope.goToLogin = function() {
+    router.goTo('public.login');
+  };
+
+  //Cleanup the modal when we're done with it!
+  $scope.$on('$destroy', function() {
+    $scope.modal.remove();
+  });
 
   /**
    * Handle register form submission and validation. Once the use has
@@ -29,7 +47,7 @@ function RegisterController($scope, router, user, errorHandler) {
     user
       .register(model)
       .then(function(data) {
-        router.goTo('public.emailSent');
+        $scope.modal.show();
       })
       .catch(function(error) {
         console.log('on error', error);
@@ -42,5 +60,6 @@ module.exports = [
   'router',
   'user',
   'errorHandler',
+  '$ionicModal',
   RegisterController
 ];

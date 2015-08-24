af9b519b60132c1793b85ff22d30ca866c655de4
lesterzone <lesterzone@gmail.com>
2015-08-24 11:23:42 -0600

    Return contacts based on API response
    Do not filter contacts where contact info starts with user input

--- www/app/components/post/post.controller.js
+++ www/app/components/post/post.controller.js
@@ -132,9 +132,13 @@
 
     ctrl.loadContacts = function($query) {
       return ContactService.all().then(function(contacts){
+        return contacts;
+        /**
         return contacts.filter(function(contact) {
-            return contact.firstName.toLowerCase().indexOf($query.toLowerCase()) != -1 || contact.lastName.toLowerCase().indexOf($query.toLowerCase()) != -1 || contact.email.toLowerCase().indexOf($query.toLowerCase()) != -1;
+            return !!contact.email;
+            // return contact.firstName.toLowerCase().indexOf($query.toLowerCase()) != -1 || contact.lastName.toLowerCase().indexOf($query.toLowerCase()) != -1 || contact.email.toLowerCase().indexOf($query.toLowerCase()) != -1;
           });
+        */
       });
     };
 

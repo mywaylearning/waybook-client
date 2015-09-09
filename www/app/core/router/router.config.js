6ec409395035d8c5775e06c3f1eaa0503c82942b
Igor Prado <igorprado@gmail.com>
2015-09-09 14:27:08 -0300

    Removed cache from discoveries tab

--- www/app/core/router/router.config.js
+++ www/app/core/router/router.config.js
@@ -267,6 +267,7 @@ function RouterConfig($stateProvider, $urlRouterProvider, $urlMatcherFactoryProv
 
   .state('app.me.discoveries', {
     url: '/discoveries',
+    cache: false,
     views: {
       'discoveries-tab': {
         templateUrl: 'app/sections/me/discoveries.tab.html',

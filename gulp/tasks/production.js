3dc012fd47666c147a30068c152e7201f89d04f4
Igor Prado <igorprado@gmail.com>
2015-11-16 19:11:04 -0200

    Upgraded CircleCI to use node v4, added favicon gulp task

--- gulp/tasks/production.js
+++ gulp/tasks/production.js
@@ -9,6 +9,6 @@ gulp.task('prod', ['clean'], function(cb) {
 
   global.isProd = true;
 
-  runSequence(['styles', 'images', 'fonts', 'views', 'browserify'], 'revision', 'revreplace', 'gzip', cb);
+  runSequence(['styles', 'images', 'fonts', 'views', 'browserify', 'favicon'], 'revision', 'revreplace', 'gzip', cb);
 
 });

45f9a244de686cbdfcd5eaa391afe6b44c0d94f0
Igor Prado <igorprado@gmail.com>
2015-12-04 16:11:00 -0200

    Fix tasks order

--- app/js/sections/guideme/guideme.controller.js
+++ app/js/sections/guideme/guideme.controller.js
@@ -29,8 +29,16 @@ function GuideMeController($scope, $state, tasks) {
     'unite': {
       state: 'app.unite.add',
       params: {}
+    },
+    'explore': {
+      state: 'app.explore.exploration',
+      params: {}
     }
   };
+
+  tasks.sort(function(a, b) {
+    return a.order - b.order;
+  });
 
   angular.forEach(tasks, function(task) {
     task._completed = task.completed;
@@ -50,7 +58,13 @@ function GuideMeController($scope, $state, tasks) {
     state = stateMap[task.section].state;
     params = stateMap[task.section].params;
 
-    params.tags = task.tags;
+    if (task.section !== 'explore') {
+      params.tags = task.tags;
+    }
+
+    if (task.section === 'explore') {
+      params.exploration = task.slug;
+    }
 
     $state.go(state, params, {});
   };

cafa3e9ad1d0f03336326e1706b85d5ea158309b
Igor Prado <igorprado@gmail.com>
2016-01-19 16:49:11 -0200

    Improved dashboard

--- app/js/sections/dashboard/dashboard.controller.js
+++ app/js/sections/dashboard/dashboard.controller.js
@@ -2,6 +2,8 @@ function DashboardController($scope, $state, dashboardData) {
   'ngInject';
 
   $scope.viewData = dashboardData;
+
+  $scope.viewData.questionsCompletedPercentage = (dashboardData.questionsCompleted / dashboardData.questions * 100).toFixed(0) + '%';
 }
 
 module.exports = DashboardController;

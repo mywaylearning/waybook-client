88f58aa5454716669c29e35abb4b329e35361ea9
Igor Prado <igorprado@gmail.com>
2016-01-27 12:40:16 -0200

    Mobile improvements, facebook native social login working

--- app/js/components/social-login/social-login.directive.js
+++ app/js/components/social-login/social-login.directive.js
@@ -1,9 +1,9 @@
 /* globals hello, facebookConnectPlugin */
-function SocialLoginDirective($ionicLoading, $q, $state, UserService) {
+function SocialLoginDirective() {
   return {
     restrict: 'E',
     templateUrl: 'components/social-login/social-login.html',
-    controller: function($scope) {
+    controller: function($scope, $ionicLoading, $q, $state, UserService, auth) {
       'ngInject';
 
       // This method is to get the user profile info from the facebook api
@@ -12,15 +12,73 @@ function SocialLoginDirective($ionicLoading, $q, $state, UserService) {
 
         facebookConnectPlugin.api('/me?access_token=' + authResponse.accessToken, null,
           function(response) {
-            console.log(response);
             info.resolve(response);
           },
           function(response) {
-            console.log(response);
             info.reject(response);
           }
         );
         return info.promise;
+      };
+
+      // This method is to get the user permissions info from the facebook api
+      var getFacebookPermissions = function(authResponse) {
+        var info = $q.defer();
+
+        facebookConnectPlugin.api('/me/permissions?access_token=' + authResponse.accessToken, null,
+          function(response) {
+            info.resolve(response);
+          },
+          function(response) {
+            info.reject(response);
+          }
+        );
+        return info.promise;
+      };
+
+      // This method is to delete user permissions in order to complete login successfully
+      var deleteFacebookPermissions = function(authResponse) {
+        var info = $q.defer();
+
+        facebookConnectPlugin.api('/me/permissions?method=delete&access_token=' + authResponse.accessToken, null,
+          function(response) {
+            info.resolve(response);
+          },
+          function(response) {
+            info.reject(response);
+          }
+        );
+        return info.promise;
+      };
+
+      // Perform login if everything is ok
+      var doLogin = function(authResponse) {
+        getFacebookProfileInfo(authResponse).then(function(profileInfo) {
+          var _user = {
+            email: profileInfo.email,
+            firstName: profileInfo.first_name,
+            lastName: profileInfo.last_name,
+            provider: 'facebook',
+            providerId: profileInfo.id,
+            auth: {
+              access_token: authResponse.accessToken
+            }
+          };
+
+          UserService.socialLoginCheck(_user).then(function(data) {
+            auth.saveAuth(data);
+            $state.go('app.main.home');
+          }).catch(function(error) {
+            if (error.error === 'not found') {
+              $state.go('public.register', {
+                userInfo: _user
+              });
+            }
+          });
+        }, function(fail) {
+          // Fail get profile info
+          console.log('profile info fail', fail);
+        });
       };
 
       // This is the success callback from the login method
@@ -31,32 +89,18 @@ function SocialLoginDirective($ionicLoading, $q, $state, UserService) {
           return;
         }
 
-        getFacebookProfileInfo(authResponse)
-          .then(function(profileInfo) {
-            var _user = {
-              email: profileInfo.email,
-              firstName: profileInfo.first_name,
-              lastName: profileInfo.last_name,
-              provider: 'facebook',
-              providerId: profileInfo.id,
-              auth: authResponse
-            };
-
-            UserService.socialLoginCheck(_user).then(function(data) {
-              auth.saveAuth(data);
-              $state.go('app.main.home');
-            }).catch(function(error) {
-              if (error.error === 'not found') {
-                $state.go('public.register', {
-                  userInfo: _user
-                });
-              }
+        getFacebookPermissions(authResponse).then(function(permissions) {
+          if (!$scope.checkPermission(permissions.data, 'email')) {
+            // User didn't authorized e-mail
+            deleteFacebookPermissions(authResponse).then(function() {
+              $scope.logout();
+              $scope.noEmail = true;
             });
-            $ionicLoading.hide();
-          }, function(fail) {
-            // Fail get profile info
-            console.log('profile info fail', fail);
-          });
+          } else {
+            doLogin(authResponse);
+          }
+          $ionicLoading.hide();
+        });
       };
 
       // This is the fail callback from the login method
@@ -68,47 +112,23 @@ function SocialLoginDirective($ionicLoading, $q, $state, UserService) {
       $scope.login = function(network, event) {
         event.preventDefault();
         if (ionic.Platform.isWebView()) {
+          $ionicLoading.show({
+            template: 'Please wait...'
+          });
+          $scope.noEmail = false;
           facebookConnectPlugin.getLoginStatus(function(success) {
-            console.log(success);
             if (success.status === 'connected') {
               // The user is logged in and has authenticated your app, and response.authResponse supplies
               // the user's ID, a valid access token, a signed request, and the time the access token
               // and signed request each expire
               fbLoginSuccess(success);
-
-              // if (!user.userID) {
-              //   getFacebookProfileInfo(success.authResponse)
-              //   .then(function(profileInfo) {
-              //   // For the purpose of this example I will store user data on local storage
-              //   // UserService.setUser({
-              //   // authResponse: success.authResponse,
-              //   // userID: profileInfo.id,
-              //   // name: profileInfo.name,
-              //   // email: profileInfo.email,
-              //   // picture : "http://graph.facebook.com/" + success.authResponse.userID + "/picture?type=large"
-              //   // });
-
-              //     $state.go('app.home');
-              //   }, function(fail) {
-              //     // Fail get profile info
-              //     console.log('profile info fail', fail);
-              //   });
-              // } else {
-              //   $state.go('app.home');
-              // }
             } else {
               // If (success.status === 'not_authorized') the user is logged in to Facebook,
               // but has not authenticated your app
               // Else the person is not logged into Facebook,
               // so we're not sure if they are logged into this app or not.
-              console.log('getLoginStatus', success.status);
-
-              $ionicLoading.show({
-                template: 'Logging in...'
-              });
 
               // Ask the permissions you need. You can learn more about
-              // FB permissions here: https://developers.facebook.com/docs/facebook-login/permissions/v2.4
               facebookConnectPlugin.login(['email', 'public_profile'], fbLoginSuccess, fbLoginError);
             }
           });
--- app/js/core/router/router.config.js
+++ app/js/core/router/router.config.js
@@ -1,4 +1,4 @@
-/* globals hello */
+/* globals hello, facebookConnectPlugin */
 function RouterConfig($stateProvider, $urlRouterProvider, $urlMatcherFactoryProvider, $locationProvider, store, ROLES, LOCAL_STORAGE_KEYS, POST_TYPES) {
   'ngInject';
   var userResolve;
@@ -101,7 +101,7 @@ function RouterConfig($stateProvider, $urlRouterProvider, $urlMatcherFactoryProv
     template: '<ion-nav-bar class="bar-stable"><ion-nav-back-button state-nav-back-button></ion-nav-back-button></ion-nav-bar><ion-nav-view name="publicContent"></ion-nav-view>',
     controllerAs: '',
     controller: function($scope, $state, UserService, auth) {
-      function checkPermission(data, permission) {
+      $scope.checkPermission = function(data, permission) {
         var toReturn = false;
         angular.forEach(data, function(item) {
           if (item.permission === permission) {
@@ -112,14 +112,14 @@ function RouterConfig($stateProvider, $urlRouterProvider, $urlMatcherFactoryProv
         });
 
         return toReturn;
-      }
+      };
 
-      function logout() {
+      $scope.logout = function() {
+        facebookConnectPlugin.logout();
         hello.logout('facebook');
         hello.logout('google');
         $scope.onLogin = false;
-        $scope.$apply();
-      }
+      };
       /**
        * https://github.com/MrSwitch/hello.js#4-add-listeners-for-the-user-login
        */
@@ -128,10 +128,10 @@ function RouterConfig($stateProvider, $urlRouterProvider, $urlMatcherFactoryProv
         $scope.onLogin = true;
         if (_auth.network === 'facebook') {
           hello(_auth.network).api('/me/permissions').then(function(response) {
-            if (!checkPermission(response.data, 'email')) {
+            if (!$scope.checkPermission(response.data, 'email')) {
               // User didn't authorized e-mail
               hello(_auth.network).api('/me/permissions', 'delete').then(function() {
-                logout();
+                $scope.logout();
                 $scope.noEmail = true;
                 $scope.$apply();
               });
@@ -139,7 +139,7 @@ function RouterConfig($stateProvider, $urlRouterProvider, $urlMatcherFactoryProv
               doLogin(_auth);
             }
           }, function() {
-            logout();
+            $scope.logout();
           });
         } else {
           doLogin(_auth);
@@ -173,7 +173,7 @@ function RouterConfig($stateProvider, $urlRouterProvider, $urlMatcherFactoryProv
           // if (_auth.network === 'facebook') {}
           // if (_auth.network === 'google') {}
         }, function() {
-          logout();
+          $scope.logout();
         });
       }
     },
@@ -460,6 +460,7 @@ function RouterConfig($stateProvider, $urlRouterProvider, $urlMatcherFactoryProv
   })
 
   .state('app.me.account', {
+    loading: true,
     url: '',
     cache: false,
     views: {
@@ -506,6 +507,7 @@ function RouterConfig($stateProvider, $urlRouterProvider, $urlMatcherFactoryProv
   })
 
   .state('app.help-feedback', {
+    loading: true,
     url: '/help-feedback',
     views: {
       'bodyContent': {

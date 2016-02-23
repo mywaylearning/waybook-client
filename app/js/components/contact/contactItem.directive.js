c2a552e7d09c222e70122685690e62857e0db6dc
Igor Prado <igorprado@gmail.com>
2016-02-23 09:20:59 -0300

    Bugfixes

--- app/js/components/contact/contactItem.directive.js
+++ app/js/components/contact/contactItem.directive.js
@@ -2,9 +2,18 @@ function contactItem() {
   return {
     restrict: 'AE',
     scope: {
-      contact: '='
+      contact: '=',
+      readOnly: '='
     },
-    templateUrl: 'components/contact/contactItem.html'
+    templateUrl: 'components/contact/contactItem.html',
+    controller: function($scope, $state) {
+      $scope.openContact = function(id) {
+        if ($scope.readOnly) {
+          return;
+        }
+        $state.go('app.unite.contact', { contactId: id });
+      };
+    }
   };
 }
 

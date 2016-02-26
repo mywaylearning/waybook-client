e3260e4d527310aa639d9d9e59709350610887f2
Igor Prado <igorprado@gmail.com>
2016-02-26 18:46:43 -0300

    Added script to create APK for release and ignore other files

---
+++ distribute.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if ! hash jarsigner || ! hash zipalign
+  then
+    echo "Commands 'jarsigner' or 'zipalign' not found. Make sure that they are in your PATH config."
+    exit 1
+fi
+
+APK_UNSIGNED_PATH="platforms/android/build/outputs/apk"
+APK_UNSIGNED_NAME="/android-release-unsigned.apk"
+APK_SIGNED_NAME="Waybook.apk"
+
+cp $APK_UNSIGNED_PATH/$APK_UNSIGNED_NAME ./
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore mywaylearning.keystore ./$APK_UNSIGNED_NAME waybook
+zipalign -v 4 ./$APK_UNSIGNED_NAME ./$APK_SIGNED_NAME
+
+rm ./$APK_UNSIGNED_NAME

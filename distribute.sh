baded975c9b8d707ffa0a7bfccebd74d03846de6
Igor Prado <igorprado@gmail.com>
2016-03-02 08:34:31 -0300

    Improved distribute script

--- distribute.sh
+++ distribute.sh
@@ -8,11 +8,10 @@ fi
 
 APK_UNSIGNED_PATH="platforms/android/build/outputs/apk"
 APK_UNSIGNED_NAME="/android-release-unsigned.apk"
-APK_SIGNED_NAME="Waybook.apk"
+APK_VERSION=$(awk '/widget/ {print $4}' FS='"' config.xml)
 
-rm ./$APK_SIGNED_NAME
 cp $APK_UNSIGNED_PATH/$APK_UNSIGNED_NAME ./
 jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore mywaylearning.keystore ./$APK_UNSIGNED_NAME waybook
-zipalign -v 4 ./$APK_UNSIGNED_NAME ./$APK_SIGNED_NAME
+zipalign -v 4 ./$APK_UNSIGNED_NAME ./"Waybook"-"$APK_VERSION".apk
 
 rm ./$APK_UNSIGNED_NAME
